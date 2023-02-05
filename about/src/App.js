90edac26aa55f78bad443a65e79704f219d244f9
diegogalviz <arqdiegogalvizv@gmail.com>
2023-02-05 17:12:58 -0500

    final de primer proyecto completo serio

--- about/src/App.js
+++ about/src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import {About} from "./components/About";
 
 function App() {
-  return (
-    <div className="App">
+    return (<div className="App">
+        <div className='container-app'>
+            <h1>Nuestro equipo</h1>
 
-    </div>
-  );
+            <About
+                name='Salomè Galviz Luna'
+                country='Colombia'
+                profession='Mujer Feliz'
+                image='2'
+                description='niña de siente años hija de DIOS'
+            />
+
+            <About
+                name='Salomè Galviz Luna'
+                country='Colombia'
+                profession='Mujer Feliz'
+                image='1'
+                description='niña de siente años hija de DIOS'
+            />
+
+            <About
+                name='Salomè Galviz Luna'
+                country='Colombia'
+                profession='Mujer Feliz'
+                image='3'
+                description='niña de siente años hija de DIOS'
+            />
+
+
+        </div>
+    </div>);
 }
 
 export default App;
--- about/src/components/About.jsx
+++ about/src/components/About.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import '../stylesheets/About.css'
 
-const About = () =>  {
-    return(
-        <div className='container-about'>
-            <img
-                src={require('../assets/img/profile1.jpg')}
+const About = (props) => {
+    return (<div className='container-about'>
+            <img className='img-profile'
+                src={require(`../assets/img/profile${props.image}.jpg`)}
                 alt="profile1"
-                />
+            />
 
             <div className='content-about'>
-                <p className='name-profile'></p>
-                <p className='profession-profile'></p>
-                <p className='description-profile'></p>
+                <p className='name-profile'>
+                    <strong>{props.name} </strong>
+                    en <strong>{props.country}</strong></p>
+                <p className='profession-profile'>{props.profession}</p>
+                <p className='description-profile'>"{props.description}"</p>
             </div>
 
-        </div>
-    )
+        </div>)
 }
+
+export {About}
