ab30b3e334571aa211433be70c200d15f95880c5
diegodev <dagalviz83@misena.edu.co>
2023-02-15 18:53:15 -0500

    todoApp completed

--- todoapp/src/components/TaskForm.jsx
+++ todoapp/src/components/TaskForm.jsx
@@ -1,28 +1,49 @@
-import React from "react";
-import '../stylesheets/TaskForm.css'
+import React, {useState} from "react";
+import '../stylesheets/TaskForm.css';
+import {v4 as uuidv4} from 'uuid';
 
 const TaskForm = (props) => {
-    return (
-        <form className='task-form'>
+    const [input, setInput] = useState('')
+
+    const manageChange = (e) => {
+        let value = e.target.value
+        setInput(value)
+        console.log(input)
+    }
+    const manageSend = (e) => {
+        e.preventDefault();
+
+        const newTask = {
+            id: uuidv4(),
+            text: input,
+            completed: false
+        }
+        props.onSubmit(newTask)
+
+    }
+
+
+    return (<form
+            onSubmit={manageSend}
+            className='task-form'>
 
             <input
                 type="text"
                 className='task-input'
                 placeholder='Escribe una tarea'
                 name='texto'
+                onChange={manageChange}
             />
 
             <button
                 className='button-task'
 
             >
-            Add task
+                Add task
             </button>
 
 
-
-        </form>
-    )
+        </form>)
 }
 
 export {TaskForm}
--- todoapp/src/components/TaskList.jsx
+++ todoapp/src/components/TaskList.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {TaskForm} from "./TaskForm";
 import '../stylesheets/TaskList.css'
-import {useState} from "react";
 import {Task} from "./Task";
 
 const TaskList = () => {
     const [tasks, setTasks] = useState([])
 
-    return(
+    const addTask = task => {
+        if (task.text.trim()) {
+            task.text = task.text.trim()
+
+            const updatedTask = [task, ...tasks]
+            setTasks(updatedTask)
+        }
+    }
+
+    const taskDelete = (id) => {
+        const updatedTask = tasks.filter(task => task.id !== id);
+        setTasks(updatedTask)
+    }
+
+    const taskCompleted = (id) => {
+        const updatedTask = tasks.map(task => {
+            if(task.id === id){
+                task.completed= !task.completed
+            }
+            return task
+        })
+        setTasks(updatedTask)
+    }
+
+
+    return (
         <>
-            <TaskForm/>
+            <TaskForm onSubmit={addTask}/>
             <div className='task-list-container'>
                 {
                     tasks.map((task) =>
-                    <Task
-                       text={task.text}
-                       completed={task.completed}
-                    />
+                        <Task
+                            key={task.id}
+                            id={task.id}
+                            text={task.text}
+                            completed={task.completed}
+                            taskCompleted={taskCompleted}
+                            taskDelete={taskDelete}
+                        />
                     )
                 }
             </div>
